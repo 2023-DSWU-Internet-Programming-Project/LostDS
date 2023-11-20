33feb1d2b769663ff5b028c7448ebfb203c42cf4
Cha Seoyeon <seoyeon0833@gmail.com>
2023-11-21 03:43:14 +0900

    리스트 페이지 html 및 css 설정, 카테고리 및 head 태그 모듈화, 이미지 업로드 기능 구현, 폼 작성 및 카테고리 연결을 위한 views.py 수정

--- project/message/views.py
+++ project/message/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 class findList(ListView):
     model = FindItem
     ordering = '-pk'
     template_name = 'message/findItem_list.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(findList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = FindItem.objects.filter(category=None).count()
+        return context
+
 class findDetail(DetailView):
     model = FindItem
     template_name = 'message/findItem_detail.html'
     # 컨텍스트 객체 이름 지정
     context_object_name = 'find'
+
+class FindPostCreate(CreateView):
+    model = FindItem
+    fields = ['title', 'content', 'head_image', 'category']
 
 class askList(ListView):
     model = AskItem
@@ -25,6 +35,10 @@ class askDetail(DetailView):
     # 컨텍스트 객체 이름 지정
     context_object_name = 'ask'
 
+class AskPostCreate(CreateView):
+    model = AskItem
+    fields = ['title', 'content', 'head_image', 'category']
+
 class completeList(ListView):
     model = CompleteItem
     template_name = 'message/completeItem_list.html'
@@ -35,12 +49,19 @@ class completeDetail(DetailView):
     # 컨텍스트 객체 이름 지정
     context_object_name = 'complete'
 
+class CompletePostCreate(CreateView):
+    model = CompleteItem
+    fields = ['title', 'content', 'head_image', 'category']
+
 def complete_post(request, pk):
     post = get_object_or_404(FindItem, pk=pk)
     
     # author 속성을 만들었을 때 추후 추가할 if문. 현재 로그인 유저 == 작성자일 경우 버튼을 누를 수 있게하기 위함.
     # if post.user == request.user:
-    completed_post = completeList.model(title=post.title, content=post.content)
+    completed_post = completeList.model(
+        title=post.title,
+        content=post.content,
+        created_at=post.created_at)
     completed_post.save()
 
     post.delete()
