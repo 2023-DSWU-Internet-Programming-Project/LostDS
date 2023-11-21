afcec20e9076a7be1c1d4976da0c5f06df8bd1d4
Cha Seoyeon <seoyeon0833@gmail.com>
2023-11-22 04:02:28 +0900

    리스트 페이지 css 수정 및 카테고리 연결 추가
    1. 리스트 페이지 카테고리 아이콘 수정 및 호버 기능 추가
    2. 게시물 클릭 시 디테일 페이지로 이동 구현
    3. 카테고리 모델 수정
    4. 카테고리 클릭 시 해당 카테고리 게시물만 보이는 기능 구현
    5. main 앱의 urls.py 및 connect.html, nav.html 수정 (url 오류의 원인)
    6. 모듈화 리셋, base.html 삭제 (find, ask, complete 페이지 각각 모델이 달라 url을 따로 설정해 주어야 했기 때문)

--- project/message/urls.py
+++ project/message/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('find/category/<str:slug>/', views.category_page_find),
+    path('complete/category/<str:slug>/', views.category_page_complete),
+    path('ask/category/<str:slug>/', views.category_page_ask),
     path('find/', views.findList.as_view()),
     path('find/<int:pk>/', views.findDetail.as_view()),
     path('ask/', views.askList.as_view()),
@@ -10,5 +13,4 @@ urlpatterns = [
     path('complete_post/<int:pk>/', views.complete_post, name='complete_post'),
     path('create_findPost/', views.FindPostCreate.as_view()),
     path('create_askPost/', views.AskPostCreate.as_view()),
-    path('create_completePost/', views.CompletePostCreate.as_view()),
 ]
