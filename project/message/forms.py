aac1a9696030c27339ddf0483fb9a2fc2d03e158
Cha Seoyeon <seoyeon0833@gmail.com>
2023-11-24 15:34:16 +0900

    [1124] 댓글 삭제 버튼 추가 및 css 수정
    1. 권한이 있는 사용자만 '완료' 버튼을 누를 수 있도록 수정
    2. 디테일 페이지 css 수정
    3. 글 쓰기, 글 수정, 글 삭제 등에 권한이 있는 사용자(관리자, 작성자)만 접근할 수 있도록 수정
    4. 디테일 뷰에서 등록한 이미지 파일이 있을 때에만 출력하도록 설정(랜덤 이미지는 출력 x)
    5. 댓글 삭제 버튼 추가
    6. 리스트 페이지 css 수정
    7. 버그 수정 (완료 게시글 내에서 게시글 삭제 버튼 작동X 버그, 댓글 삭제 버튼을 누를 시 url 로딩 오류 버그, 분실물 리스트에서 글 쓰기 버튼 동작X 버그)

--- project/message/forms.py
+++ project/message/forms.py
@@ -2,6 +2,11 @@ from .models import Comment
 from django import forms
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={'placeholder': ' 댓글을 입력해주세요 . . .','style': 'width: 100%; height: 100px; border-radius: 10px; resize: none;'})
+    )
+
     class Meta:
         model = Comment
         fields = ('content',)
