68c603be30165d77502050e409ea5c39c299db7a
gyurili <inglifestora@naver.com>
2023-11-25 01:58:20 +0900

    이메일 인증 추가

--- project/project/settings.py
+++ project/project/settings.py
@@ -135,5 +135,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = '/login/'
 
+# send email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dsforemailtest@gmail.com'
+EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
 
-
+AUTH_USER_MODEL = 'user.CustomUser'
--- project/user/urls.py
+++ project/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'user'
+
 urlpatterns = [
-    path("", login_view),
+    path("", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("signup/", signup_view),
+    path("verification/", verification_view, name="verification"),
 ]
