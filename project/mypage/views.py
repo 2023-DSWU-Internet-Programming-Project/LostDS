a5612257f3cdefc4e54ab5d40389537652a64ccb
Cha Seoyeon <seoyeon0833@gmail.com>
2023-12-04 16:44:21 +0900

    충돌 해결

--- project/mypage/views.py
+++ project/mypage/views.py
@@ -1,32 +1,29 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required   # 아이디와 이메일 마이페이지에 가져오기 위해 사용했음
-<<<<<<< HEAD
-=======
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required  # 아이디와 이메일 마이페이지에 가져오기 위해 사용했음
 from message.models import AskItem, FindItem, Comment
+
 from django.views.generic import ListView
->>>>>>> dd6dfbc074a5409316637634a5b4df46b85c0a1c
+
 
 def mypage_view(request):
     # 사용자 정보를 세션에서 가져와서 전달
     username = request.user.username
     email = request.user.email
 
-    return render(request, 'mypage/mypage.html', {'username':username, 'email':email, })
+    return render(request, 'mypage/mypage.html', {'username': username, 'email': email, })
 
-def mypost_view(requset):   # 게시글 리스트 페이지
+
+def mypost_view(requset):  # 게시글 리스트 페이지
 
     return render(
         requset,
         'mypage/my-post.html',
     )
 
-def comment_view(requset):   # 댓글 리스트 페이지
+
+def comment_view(requset):  # 댓글 리스트 페이지
 
     return render(
         requset,
         'mypage/comment.html',
-<<<<<<< HEAD
     )
-=======
-    )
->>>>>>> dd6dfbc074a5409316637634a5b4df46b85c0a1c
