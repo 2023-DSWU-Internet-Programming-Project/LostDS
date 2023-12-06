86ec989ae6e10269eea632f4cc3df27eb02b5b84
gyurili <inglifestora@naver.com>
2023-12-06 15:25:50 +0900

    회원가입 시 인증 토큰 만료 설정

--- project/user/models.py
+++ project/user/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
 class CustomUser(AbstractUser):
     get_verification_token = models.CharField(max_length=64, blank=True, null=True)
+    token_created_at = models.DateTimeField(null=True)
 
     groups = models.ManyToManyField('auth.Group', related_name='custom_users')
     user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_users')
